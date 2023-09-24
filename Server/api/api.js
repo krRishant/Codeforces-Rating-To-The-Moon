e369964572373a48933d6db8b0790a936d7fc13d
krRishant <kumarrkrishant@gmail.com>
2023-09-24 14:25:20 +0530

    created question list for all ratings

--- Server/api/api.js
+++ Server/api/api.js
@@ -15,69 +15,87 @@ rating['grandmaster'] = "international grandmaster";
 rating['international grandmaster'] = "legendary grandmaster";
 rating['legendary grandmaster'] = "you done bro";
 
+let rating_list = [
+     'pupil', 'specialist',
+    'expert', 'candidate master','master',
+    'international master', 'grandmaster', 'international grandmaster',
+    'legendary grandmaster'
+
+]
 
 const question_list = async (curr_rating) => {
 
-    let next_rating = rating[curr_rating];
-    console.log(next_rating);
-    const users = usersWithRating[next_rating].map(users => { return users.username });
+    const users = usersWithRating[curr_rating].map(users => { return users.username });
     let currentTime = Math.round(new Date().getTime() / 1000);
 
     // Create an object to store the frequency of solved problems
     let pFreq = new Map([]);
 
-    // Get the first user
-
-    let user = "Rishant_07";
-    console.log(user);
-
     // Get the submission history of the  first 10 users
-
-    axios
-        .get(`https://codeforces.com/api/user.status?handle=${user}`)
-        .then((response) => {
+    async function getSubmissionHistory(user) {
+        try {
+            const response = await axios.get(`https://codeforces.com/api/user.status?handle=${user}`);
             let data = response.data;
             // console.log(data.result[0]);
             // Filter the submissions to include only those made in the last 6 months and with a verdict of "OK"
             let solvedSubmissions = data.result.filter(
                 (submission) =>
                     submission.verdict === "OK" &&
-                    submission.creationTimeSeconds >= currentTime - 1 * 30 * 24 * 60 * 60
+                    submission.creationTimeSeconds >= currentTime - 6 * 30 * 24 * 60 * 60
             );
             // Extract the problem IDs and tags as array of objects from the filtered submissions
             solvedSubmissions.forEach(
                 (submission) => {
-                    if(pFreq.has(submission.problem.contestId + submission.problem.index)){
+                    if (pFreq.has(submission.problem.contestId + submission.problem.index)) {
                         let curr_obj = pFreq.get(submission.problem.contestId + submission.problem.index);
                         let updated_obj = {
-                            frequency: curr_obj.frequency+1,
+                            frequency: curr_obj.frequency + 1,
                             tags: curr_obj.tags
                         }
-                        pFreq.set(submission.problem.contestId + submission.problem.index,updated_obj);
-                    }else {
-                        pFreq.set(submission.problem.contestId + submission.problem.index,{
+                        pFreq.set(submission.problem.contestId + submission.problem.index, updated_obj);
+                    } else {
+                        pFreq.set(submission.problem.contestId + submission.problem.index, {
                             frequency: 1,
-                            tags:submission.problem.tags
-                        });    
+                            tags: submission.problem.tags
+                        });
                     }
                 }
             );
-            
-            console.log("Map", pFreq);
-            // console.log(solvedProblems);
-            // Update the frequency of solved problems as  problemId : { frequency:   ,  tags: []}
 
 
-            // sort the updatedSolvedProblems array by frequency in descending order
-            // const sortedSolvedProblems = solvedProblems.sort((a, b) => b.frequency - a.frequency);
-
-            // console.log("solved problem",sortedSolvedProblems);
-        })
-        .catch((error) => {
+        } catch (error) {
             // Handle error
             console.log(error);
-        });
+        }
+    };
+    for (var i = 0; i < Math.min(users.length, 1); i++) {
+        let user = users[i];
+        await getSubmissionHistory(user);
+    }
+    // console.log("myMap",pFreq);
+    let solvedProblems = [];
+    for (let [key, value] of pFreq) {
+        solvedProblems.push({
+            problemId: key,
+            frequency: value.frequency,
+            tags: value.tags
 
+        })
+        // console.log(key + " = " + value);
+    }
+    // sort on the basis of frequency 
+    const sortedSolvedProblems = solvedProblems.sort((a, b) => b.frequency - a.frequency);
+
+
+    console.log("findal solvedProblemsay, ", sortedSolvedProblems);
+
+
+    const jsonData = {
+        [curr_rating]: sortedSolvedProblems
+    }
+    const jsonString = JSON.stringify(jsonData,null,2);
+
+    fs.writeFileSync(`./questions/${curr_rating}.json`,jsonString);
 
 
 };
@@ -90,9 +108,13 @@ const getallUser = async () => {
     try {
         var response = await axios.get('https://codeforces.com/api/user.info?handles=coder_ravan');
         let user_rank = response.data.result[0].rank;
-        console.log(colors.cyan(response.data.result[0].rank));
-
-        question_list("newbie");
+        console.log(colors.cyan(user_rank));
+        // console.log(rating_list);
+        question_list('pupil');
+        // for (let key in rating_list) {
+        //     // console.log(colors.blue(rating_list[key]));
+        //     question_list(rating_list[key]);
+        // }
 
 
     } catch (error) {
